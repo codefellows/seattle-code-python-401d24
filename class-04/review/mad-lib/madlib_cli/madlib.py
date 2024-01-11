1137042ebbdcdf68e5f7f4b867ba2af9b9196557
JB Tellez <jb.tellez@gmail.com>
2024-01-11 15:34:19 -0800

    adds runnable game code

--- class-04/review/mad-lib/madlib_cli/madlib.py
+++ class-04/review/mad-lib/madlib_cli/madlib.py
@@ -1,12 +1,13 @@
 def read_template(path):
-  """
-  takes in a path to a tempate file
-  and returns the stripped text
-  """
+    """
+    takes in a path to a tempate file
+    returns the stripped text
+    """
 
-  with open(path) as file:
-    contents = file.read()
-    return contents.strip()
+    with open(path) as file:
+        contents = file.read()
+        return contents.strip()
+
 
 def parse_template(template):
     parts = []
@@ -32,5 +33,40 @@ def parse_template(template):
 
     return stripped, tuple(parts)
 
-def merge(stripped_template, parts):
-    return stripped_template.format(*parts)
+
+def merge(stripped_template, responses):
+    return stripped_template.format(*responses)
+
+def main():
+     # run the game
+
+    template = read_template("assets/dark_and_stormy_night_template.txt")
+
+    stripped_template, parts = parse_template(template)
+
+    responses = gather_responses(parts)
+
+    # merge once we have the user's responses
+    merged = merge(stripped_template, responses)
+
+    print(merged)
+
+    with open("assets/dark_and_stormy_night_completed.txt", "w+") as file:
+        file.write(merged)
+
+def gather_responses(parts):
+
+    responses = []
+
+    for part in parts:
+        response = input(f"Enter a {part}: ")
+        responses.append(response)
+
+    return responses
+
+if __name__ == "__main__":
+    # check to see if running as a script
+    # if this check is missing then it will mess up pytest
+    main()
+
+
