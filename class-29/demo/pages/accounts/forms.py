ab607a9783a964384da1065b5810070e02f52871
JB Tellez <jb.tellez@gmail.com>
2024-02-15 00:03:31 -0800

    updates demo 29 to latest Django

---
+++ class-29/demo/pages/accounts/forms.py
@@ -0,0 +1,16 @@
+# accounts/forms.py
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ("username", "email")
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ("username", "email")
