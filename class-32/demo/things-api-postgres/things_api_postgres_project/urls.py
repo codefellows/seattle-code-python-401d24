321d9479154c0afcadadce6cb8c918dd24b9c092
JB Tellez <jb.tellez@gmail.com>
2024-02-19 23:46:08 -0800

    add rest_framework urls

--- class-32/demo/things-api-postgres/things_api_postgres_project/urls.py
+++ class-32/demo/things-api-postgres/things_api_postgres_project/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/things/', include('things.urls')),
+    path("api-auth/", include("rest_framework.urls")),
 ]
