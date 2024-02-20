3c487943134876bf497ce49cc639650a6b8f76d9
JB Tellez <jb.tellez@gmail.com>
2024-02-20 12:24:32 -0800

    adds in class postgres demo tweaks

--- class-32/demo/things-api-postgres/things_api_postgres_project/settings.py
+++ class-32/demo/things-api-postgres/things_api_postgres_project/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-e6wykvxtt*0z2edwdlc6tp%!tengthe&j4=+2-_!(hwc2u5*qf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+  '0.0.0.0',
+  'localhost',
+  '127.0.0.1',
+]
 
 
 # Application definition
