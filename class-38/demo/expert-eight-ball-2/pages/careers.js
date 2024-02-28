e2c9a371a418f8419c562e8095c5ca3c5767c651
JB Tellez <jb.tellez@gmail.com>
2024-02-27 23:37:36 -0800

    updates demo 38 to latest

--- class-38/demo/expert-eight-ball-2/pages/careers.js
+++ class-38/demo/expert-eight-ball-2/pages/careers.js
@@ -5,8 +5,8 @@ export default function Careers() {
         <div className="flex items-center justify-center mx-auto my-8 bg-gray-500 w-72 h-72 text-gray-50">
             <section className="space-y-4">
                 <h1 className="text-4xl">Careers Page</h1>
-                <Link href="/">
-                    <a className="block text-center underline">Return Home</a>
+                <Link href="/" className="block text-center underline">
+                    Return Home
                 </Link>
             </section>
         </div>
